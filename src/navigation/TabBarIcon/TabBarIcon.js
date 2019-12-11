41f8e8a9b061af0ef4cadd83ff341272699e2827
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2019-12-11 15:04:58 +0200

    full-width table

--- src/navigation/TabBarIcon/TabBarIcon.js
+++ src/navigation/TabBarIcon/TabBarIcon.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Octicons from 'react-native-vector-icons/Octicons'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 export default function TabBarIcon({ tintColor, routeName }) {
 	if (routeName === 'Spendings') {
-		return <FontAwesome5 name={'money-check-alt'} size={30} color={tintColor}/>
+		return <FontAwesome name={'money'} size={30} color={tintColor}/>
 	} else if (routeName === 'Challenges') {
-		return <MaterialCommunityIcons name={'playlist-star'} size={45} color={tintColor} style={{ marginLeft: 3 }}/>
+		return <MaterialCommunityIcons name={'playlist-check'} size={45} color={tintColor} style={{ marginLeft: 3 }}/>
 	} else if (routeName === 'Wishlist') {
 		return <Octicons name={'checklist'} size={30} color={tintColor} style={{ marginLeft: 8 }}/>
 	}
